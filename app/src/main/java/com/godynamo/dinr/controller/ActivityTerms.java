d6c4d1a32bb0482afb61f81b541fde49a516080d
Ashish <ashish@appincubator.io>
2022-04-07 13:12:34 +0530

    updated web pages

--- app/src/main/java/com/godynamo/dinr/controller/ActivityTerms.java
+++ app/src/main/java/com/godynamo/dinr/controller/ActivityTerms.java
@@ -2,19 +2,17 @@ package com.godynamo.dinr.controller;
 
 import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.core.content.ContextCompat;
+
 import com.godynamo.dinr.R;
 import com.godynamo.dinr.api.EndPointUrl;
 import com.godynamo.dinr.tools.Utils;
 
-/**
- * Created by dankovassev on 2015-02-05.
- */
 public class ActivityTerms extends BaseDinrActivity{
 
     @Override
@@ -26,19 +24,14 @@ public class ActivityTerms extends BaseDinrActivity{
         actionBar.setCustomView(R.layout.action_bar_back_button);
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 
-        TextView textView = (TextView)actionBar.getCustomView().findViewById(R.id.custom_action_bar_title);
+        TextView textView = actionBar.getCustomView().findViewById(R.id.custom_action_bar_title);
         textView.setText(this.getString(R.string.terms_of_service));
 
-        ImageView backButton = (ImageView)actionBar.getCustomView().findViewById(R.id.back_button);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        ImageView backButton = actionBar.getCustomView().findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> finish());
 
-        WebView webview = (WebView) findViewById(R.id.term_webview);
-        webview.setBackgroundColor(getResources().getColor(R.color.black));
+        WebView webview = findViewById(R.id.term_webview);
+        webview.setBackgroundColor(ContextCompat.getColor(this, R.color.black));
         webview.loadUrl(Utils.returnLocalizedUrl(EndPointUrl.DINR_TERMS_URL));
 
     }
